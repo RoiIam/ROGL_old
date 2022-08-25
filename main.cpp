7ff07d33979ec6cce5da0038971c88bda1b20c7d
RoiIam <robkokica@gmail.com>
2022-08-25 17:18:46 +0200

    use static GLFW, move from GLEW to GLAD, cant get assimp to work as static either

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+//#define GLEW_STATIC //couldnt buld static mingqw lib and link it properly so wr use glad
+//#include <GL/glew.h>
 
 #include <assimp/postprocess.h>
 #include <assimp/scene.h>
 #include <assimp/Importer.hpp>
-#include <glew.h>
-#include <GLFW/glfw3.h>
 #include <glm/gtc/matrix_transform.hpp>
 #include "imgui.h"
 
@@ -28,7 +30,9 @@ int main() {
     }
     glfwMakeContextCurrent(window);
 
-    GLenum err = glewInit();
+    //glad needs to be after make context current window
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        return-111;//THROW_ERROR("Failed to load functions!");
     std::cout << "Hello, World!" << std::endl;
     ::Help();
     Assimp::Importer importer;
