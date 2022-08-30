e18800dd3f169d79ef02f2d49cc7f2f57bc6f8d7
RoiIam <robkokica@gmail.com>
2022-08-30 15:43:25 +0200

    added shader model mesh cpp source files,
    modified them to use headers as should be
    added stb_image as includes

---
+++ Source/model.h
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+class Model {
+public:
+
+
+
+
+
+    // model data
+    std::vector<Texture> textures_loaded;	// stores all the textures loaded so far, optimization to make sure textures aren't loaded more than once.
+    std::vector<Mesh>    meshes;
+    std::string directory;
+    bool gammaCorrection;
+
+
+    glm::vec3 boundMin = glm::vec3 (100000,100000,100000);
+    glm::vec3 boundMax = glm::vec3 (-100000,-100000,-100000);
+
+
+    Model();
+     explicit Model(std::string const &path, bool gamma);
+    virtual ~Model();
+    virtual void Draw(Shader &shader, bool simple);
+
+
+private:
+    // loads a model with supported ASSIMP extensions from file and stores the resulting meshes in the meshes vector.
+    void loadModel(std::string const &path);
+    // processes a node in a recursive fashion. Processes each individual mesh located at the node and repeats this process on its children nodes (if any).
+    void processNode(aiNode *node, const aiScene *scene);
+
+    Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+    // checks all material textures of a given type and loads the textures if they're not loaded yet.
+    // the required info is returned as a Texture struct.
+    std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName);
+
+    unsigned int TextureFromFile(const char *path, const std::string &directory, bool gamma = false);
+};
--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@
 #include <assimp/Importer.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include "imgui.h"
+#include "Source/shader.h"
 
 #include <Log.h>
-#include <test.h>
+#include "test.h"
 
 
 int main() {
@@ -20,10 +21,13 @@ int main() {
 
     if (! glfwInit())
         return -1;
-
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (! window)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
 
         return -1;
@@ -32,12 +36,33 @@ int main() {
 
     //glad needs to be after make context current window
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-        return-111;//THROW_ERROR("Failed to load functions!");
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
     std::cout << "Hello, World!" << std::endl;
     ::Help();
     Assimp::Importer importer;
 
+    glViewport(0, 0, 800, 600);
+    Shader* light_shader;
+    //Model* lightCube;
+    light_shader = new Shader(R"(C:\Users\robko\Documents\CLion\OGLR\Assets\Shaders\MultipleLights\s_light.vert)",
+                              R"(C:\Users\robko\Documents\CLion\OGLR\Assets\Shaders\MultipleLights\s_light.frag)");
 
-    getchar();
+    while(!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    delete light_shader;
+
+    return 0;
+
     return 0;
 }
